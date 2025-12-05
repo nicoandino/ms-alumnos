79113c96a6e224448e714aaec7b2f6aa43f4ecfb
Matu <matias.e.lopez747@gmail.com>
2025-12-04 21:46:17 -0300

    arreglo test app

--- test/test_app.py
+++ test/test_app.py
@@ -1,13 +1,15 @@
 import unittest
+import os
+
+os.environ["FLASK_CONTEXT"] = "testing"
+os.environ["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+
 from flask import current_app
 from app import create_app
-import os
 
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
-        os.environ["FLASK_CONTEXT"] = "testing"
-        os.environ["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
         self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
